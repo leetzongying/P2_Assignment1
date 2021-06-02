beca2d046eeffdbf64e2f92cf25c4e3c866a3c1f
leetzongying <73087882+leetzongying@users.noreply.github.com>
2021-06-02 22:22:28 +0800

    Update Employee.java

--- Assignment1/src/travel_transport/Employee.java
+++ Assignment1/src/travel_transport/Employee.java
@@ -2,15 +2,15 @@ package travel_transport;
 
 import java.util.*;
 
-public class Employee {
-
+public class Employee { //1.3 User defined class
+        //1.2 Predefined class
 	Scanner in = new Scanner(System.in);
 	
 	String name, position, ID;
 	int hour;
 	double salary;
 	
-	public Employee() {
+	public Employee() { //1.4 Constructor with zero argument
 		System.out.println("Enter name of employee:");
 		this.name = in.nextLine();
 		System.out.println("Enter name of position:");
@@ -23,7 +23,7 @@ public class Employee {
 		this.hour = in.nextInt();
 	}
 	
-	public Employee(String p) {
+	public Employee(String p) { //1.4 Constructor with one argument
 		this.position = p;
 
 		System.out.println("Enter name of employee:");
@@ -46,7 +46,7 @@ public class Employee {
 		}
 	}
 	
-	public Employee(String p, double s) {
+	public Employee(String p, double s) { //1.4 Constructor with 2 arguments
 		this.position = p;
 		this.salary = s;
 		
